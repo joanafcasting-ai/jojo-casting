f6c59e049a3e74a53033ee23af64d89917dba9a1
joanafcasting-ai <joanafcasting@gmail.com>
2026-03-01 17:40:18 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CastingApp from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CastingApp />
-  </React.StrictMode>,
-)
